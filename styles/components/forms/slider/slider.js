cf7ebe3b9e5f15bbb15518d3ec42355f61e8bd17
Juho Gebhard <juho.gebhard@druid.fi>
2016-12-14 23:19:17 +0700

    Slider update (js, scss, twig)

--- styles/components/forms/slider/slider.js
+++ styles/components/forms/slider/slider.js
@@ -1,16 +1,20 @@
-
 $(function() {
   $('.js-slider').each(function() {
     var min = $(this).data('min');
     var max = $(this).data('max');
-    var $slider = $(this).children('.js-slider-path');
-    var $handle = $slider.children('.js-slider-handle');
+    var $slider = $(this).find('.js-slider-path');
+    var $input = $(this).find('input');
+    var $handle = $slider.find('.js-slider-handle');
     $slider.slider({
-      create: function() {
+      value: $input.val(),
+      create: function () {
         $handle.text($(this).slider('value'));
       },
       slide: function (event, ui) {
         $handle.text(ui.value);
+      },
+      change: function (event, ui) {
+        $input.val(ui.value);
       },
       min: min,
       max: max
